5198f8b26e8b0ad6b8fadc6bb0581381a27cc24f
jiangchuan <jiangchuan@ifeng.com>
2020-11-17 16:58:44 +0800

    增加prometheus相关逻辑

--- src/main/java/com/ifeng/fhh/gateway/route/RouteConfiguration.java
+++ src/main/java/com/ifeng/fhh/gateway/route/RouteConfiguration.java
@@ -20,7 +20,7 @@ import reactor.core.publisher.Mono;
 public class RouteConfiguration {
 
 
-    @Bean
+    //@Bean
     public RouteLocator myLbRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 // Add a simple re-route from: /get to: http://httpbin.org:80
