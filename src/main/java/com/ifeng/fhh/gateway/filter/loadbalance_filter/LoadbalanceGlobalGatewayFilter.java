3525584de880bdd5b31ed1782685118065f0ba03
jiangchuan <jiangchuan@ifeng.com>
2020-11-18 19:35:43 +0800

    重构loadbalancer的生产逻辑，采用抽象工厂来实现

--- src/main/java/com/ifeng/fhh/gateway/filter/loadbalance_filter/LoadbalanceGlobalGatewayFilter.java
+++ src/main/java/com/ifeng/fhh/gateway/filter/loadbalance_filter/LoadbalanceGlobalGatewayFilter.java
@@ -54,7 +54,7 @@ public class LoadbalanceGlobalGatewayFilter implements GlobalFilter, Ordered {
     @PostConstruct
     public void initLoadBalancer(){
         this.loadBalancer = abstractLoadBalancerFactory.buildLoadBalancer(); /*从工厂获取实例*/
-        LOGGER.info("init loadBalancer");
+        LOGGER.info("init loadBalancer {} ", loadBalancer);
     }
 
     @Override
