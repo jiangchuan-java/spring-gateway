ef81cff84cecb715f09fc0c6156ff6d3af7ee410
jiangchuan <jiangchuan@ifeng.com>
2020-12-11 20:17:39 +0800

    大改动，增加了很多聚合层和解耦层

--- src/main/java/com/ifeng/fhh/gateway/filter/security_filter/authorization/RoleInfoValidator.java
+++ src/main/java/com/ifeng/fhh/gateway/filter/security_filter/authorization/RoleInfoValidator.java
@@ -4,15 +4,24 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.ifeng.fhh.gateway.util.GatewayPropertyUtil;
 import com.ifeng.fhh.gateway.util.httpclient.HttpClientTemplate;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * @Des:
@@ -23,18 +32,35 @@ import java.util.Objects;
 @Component
 public class RoleInfoValidator {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RoleInfoValidator.class);
     @Autowired
     private CompositeRoleInfoRepository repository;
 
     @Autowired
     private HttpClientTemplate httpClientTemplate;
 
+    private static CircuitBreaker breaker;
+
+    private static final CircuitBreakerConfig defaultBreakerConfig = CircuitBreakerConfig.custom()
+            .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED) /*固定大小，不做限流就简单点*/
+            .slidingWindowSize(100) /*每100次计算一次,如果是时间类型的：单位就是秒*/
+            .minimumNumberOfCalls(100) /*最少调用100次才能进行统计*/
+            .failureRateThreshold(80) /*80%失败率*/
+            .waitDurationInOpenState(Duration.ofSeconds(30)) /*维持熔断状态30秒*/
+            .permittedNumberOfCallsInHalfOpenState(20) /*半打开状态下，尝试多少次请求*/
+            .build();
+
+    static {
+        CircuitBreakerRegistry registry = CircuitBreakerRegistry.of(defaultBreakerConfig);
+        breaker = registry.circuitBreaker("RoleInfoValidator");
+    }
+
     @Value("${authority_management_system_url}")
     private String authority_management_system_url;
 
-    public Mono<Boolean> validate(String serverId, String uri, String token){
+    public Mono<Boolean> validate(String serverId, String uri, String token) {
         String roleId = repository.matchRoleId(serverId, uri);
-        if(Objects.isNull(roleId)){
+        if (Objects.isNull(roleId)) {
             return Mono.just(true);
         } else {
             return checkToken(token, roleId);
@@ -42,21 +68,37 @@ public class RoleInfoValidator {
     }
 
     private Mono<Boolean> checkToken(String token, String roleId) {
-        if(Objects.isNull(token)){
+        if (Objects.isNull(token)) {
             return Mono.just(false);
         }
         Map<String, String> headers = new HashMap<>();
         headers = new HashMap<>();
         headers.put(GatewayPropertyUtil.AUTHORITY_MANAGEMENT_SYSTEM_TOKEN, token);
-        return httpClientTemplate.get(authority_management_system_url, headers, 2000,500,500)
-                .map(resp -> checkRole(resp, roleId));
+        long start = System.nanoTime();
+
+        return httpClientTemplate.get(authority_management_system_url, headers, 2000, 500, 500)
+                .onErrorResume(new Function<Throwable, Mono<String>>() {
+            @Override
+            public Mono<String> apply(Throwable throwable) {
+                long durationInNanos = System.nanoTime() - start;
+                breaker.onError(durationInNanos, TimeUnit.NANOSECONDS, throwable);
+                LOGGER.error("{} get roleId from authority system error ",authority_management_system_url, throwable);
+                return Mono.error(throwable);
+            }
+        }).doOnSuccess(new Consumer<String>() {
+            @Override
+            public void accept(String string) {
+                long durationInNanos = System.nanoTime() - start;
+                breaker.onSuccess(durationInNanos, TimeUnit.NANOSECONDS);
+            }
+        }).map(resp -> checkRole(resp, roleId));
     }
 
     private boolean checkRole(String resp, String roleId) {
         JSONObject jsonObject = JSON.parseObject(resp);
         JSONObject dataJson = jsonObject.getJSONObject("data");
         if (dataJson.containsKey("role")) {
-            List<String> list =  dataJson.getJSONArray("role").toJavaList(String.class);
+            List<String> list = dataJson.getJSONArray("role").toJavaList(String.class);
             for (String role : list) {
                 if (Objects.equals(role, roleId)) {
                     return true;
